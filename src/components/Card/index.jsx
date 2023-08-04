1db696542ef3bab3e21f2325d7461f1361302c2a
janetca30 <janetca30@gmail.com>
2023-08-04 12:54:10 -0300

    :hammer_and_pick: Design change

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,14 +5,16 @@ import './style.css'
 function Card ({title,text}) {
   return (
     <>
-      <div>
-        <h3 className='text-2xl font-semibold text-center mt-5'>{title}</h3>
-        <p className='text-m text-justify mt-3 mr-10'>{text}</p>
-        <button className='more-button bg-teal-600 font-semibold mt-5 ml-20'>
-          View More
-        </button>
+      <div className='card-pr inline-flex'>
+        <div>
+          <h3 className='text-2xl font-bold text-center mt-5'>{title}</h3>
+          <p className='text-m text-justify mt-3 mr-10'>{text}</p>
+          <button className='more-button bg-teal-600 font-semibold rounded mt-5 ml-20'>
+            View More
+          </button>
+        </div>
+        <img src={Larcomar} alt="Larcomar" />
       </div>
-      <img src={Larcomar} alt="Larcomar" />
     </>
   );
 }
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import user from '../../assets/img/user.png'
 import './style.css'
 
 function Login () {
   return (
-    <button className='login-button bg-black' type="button">
-      login 
-    </button>
+    <>
+      <button className='login-button bg-black rounded-lg inline-flex items-center' type="button">
+        <img className='login-user' src={user} alt="User" />
+        login 
+      </button> 
+    </>
+    
   );
 }
 
