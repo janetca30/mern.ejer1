fe9b065799fd1cba3d4f7d56e57035e5ec15c8d8
janetca30 <janetca30@gmail.com>
2023-08-23 13:32:30 -0300

    Last change

--- src/pages/Cities/index.jsx
+++ src/pages/Cities/index.jsx
@@ -1,40 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import TravelCard from '../../components/TravelCard/index.jsx'; // Ajusta la ruta según la ubicación del componente
+import { useState, useEffect } from 'react';
+import CityCard from '../../components/CityCard/index.jsx';
+import { getAllCities } from '../../services/citiesQueries.js';
 
+const Cities =()=> {
+  const [citiesList , setCitiesList] = useState([]);
+  useEffect(() =>{
+    getAllCities()
+    .then((data) => setCitiesList(data))
+    .catch((err) => console.log(err));
+  }, [] );
 
-function App() {
-  const [citiesData, setCitiesData] = useState([]);
-
-  useEffect(() => {
-    async function fetchCitiesData() {
-      try {
-        const response = await axios.get('http://localhost:4000/api/cities');
-        setCitiesData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching travel data:', error);
-      }
-      
-    }
-
-    fetchCitiesData();
-
-    
-  }, []);
 
   return (
     <div>
-      <h1>Travel Destinations</h1>
+      <h1>Travel </h1>
       <div className="travel-card-list">
-      {console.log(citiesData.name)}
-        {citiesData.map(destination => (
-          <TravelCard
-            key={destination.id}
-            name={destination.name}
-            country={destination.country}
-            photo={destination.photo}
-            description={destination.description}
+      
+        {citiesList.map((city) => (
+          <CityCard
+            key={city._id}
+            name={city.name}
+            country={city.country}
+            photo={city.photo}
+            description={city.description}
           />
         ))}
       </div>
@@ -42,7 +30,7 @@ function App() {
   );
 }
 
-export default App;
+export default Cities;
 
 
 
